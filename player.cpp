5241fdcf99609f435db29670ec9873c87763436d
JoonasKatainen <H3248@student.jamk.fi>
2014-11-26 15:42:51 +0200

    Hypyn säätöä

--- player.cpp
+++ player.cpp
@@ -24,17 +24,28 @@ void Player::update(float dt){
 
 		if (Controls::get()->iskeydown(sf::Keyboard::Space)){
 			this->setDY(-1);
+			if (velocityY < 500){
+				velocityY = velocityY + 100;
+				
+			}
+			falling = true;
 		}
 		if (Controls::get()->kIsReleased(sf::Keyboard::Space)){
+			if (falling){
+				if (DY == -1)
+				velocityY = velocityY - 100;
+				if (velocityY==0)
+				this->setDY(1);
+				if (DY == 1 && velocityY < 500)
+					velocityY = velocityY + 100;
 
-			this->setDY(1);
-		
+			}
 		}
 	
 	if (sprite.getPosition().y >= 400){
-
+		falling = false;
 		this->setDY(0);
-		
+		sprite.setPosition(sprite.getPosition().x, 390);
 	}
 	
 	this->move(dt);
