c5056da343a627a2aff9e9f9eac1b705bfd1084e
JoonasKatainen <H3248@student.jamk.fi>
2014-12-01 15:16:03 +0200

    Spooky ghost update 1.2
    teleporttailevia kummituksia, fukd up hitboxit

--- Enemy.cpp
+++ Enemy.cpp
@@ -4,7 +4,7 @@ Enemy::Enemy(){
 
 }
 
-void Enemy::update(float dt,float playerposX, float playerposY){
+void Enemy::update(float dt, float playerposX, float playerposY, Map &ptr, sf::View view){
 	switch (type){
 	case 0:
 		
@@ -27,6 +27,7 @@ void Enemy::update(float dt,float playerposX, float playerposY){
 		}
 		this->move(dt);
 		hitbox.update(sprite);
+		checkCollision(ptr, view);
 		break;
 	default:
 		break;
@@ -52,10 +53,64 @@ void Enemy::create(float posX, float posY, int type){
 	default:
 		break;
 	}
-	/*this->createCharacter(posX, posY, sizeX, sizeY);
 	
-	this->sprite.setTexture(Content::get()->enemyTexture);*/
-	//this->setTexture(&Content::get()->playerTexture);
+}
+void Enemy::applyGravity(){
+	if (falling){
+		if (this->DY == -1)
+			this->velocityY = this->velocityY - 50;
+		if (this->velocityY <= 0)
+			this->setDY(1);
+		if (this->DY == 1 && this->velocityY < 500)
+			this->velocityY = this->velocityY + 50;
+	}
+}
+void Enemy::jump(){
+	
+	if (this->DY == 0){
+		if (this->velocityY <= 0){
+			this->setDY(-1);
+		}
+		falling = true;
+	}
+	if (!falling){
+		if (this->velocityY < 500 && this->DY == -1){
+			this->velocityY = 500;
+		}
+	}
+	
+}
+void Enemy::checkCollision(Map &ptr, sf::View view){
+
+
+	for (int i = 0; i < ptr.mapObjects.size(); i++){
+		if ((ptr.mapObjects[i]->getPos().x - sprite.getPosition().x*(ptr.mapObjects[i]->getPos().x - sprite.getPosition().x)  < 3000 && (ptr.mapObjects[i]->getPos().y - sprite.getPosition().y)*(ptr.mapObjects[i]->getPos().y - sprite.getPosition().y) < 3000)) // tehoja lisää
+		{
+			if (!ptr.mapObjects[i]->passable){
+
+				if (Utility::boxHit(this->hitbox.fullHitbox, ptr.mapObjects[i]->shape)){
+						sprite.setPosition(sprite.getPosition().x, ptr.mapObjects[i]->getPos().y - this->sizeY / 2 + 1);
+						sprite.setPosition(ptr.mapObjects[i]->getPos().x + ptr.mapObjects[i]->getSize().x + this->sizeX / 2 - 1, sprite.getPosition().y);
+						this->setDY(0);
+						this->velocityY = 0;
+						this->setDX(0);
+						this->velocityX = 0;
+				}
+
+				//else if (Utility::boxHit(this->hitbox.bodyRightHitbox, ptr.mapObjects[i]->shape)){
+					//	sprite.setPosition(ptr.mapObjects[i]->getPos().x - this->sizeX / 2 + 1, sprite.getPosition().y);
+					//this->bodyRightHitbocCollides = true;
+				//}
+			}
+		}
+	}
+
+	
+		
+	
+	
+		hitbox.update(sprite);
+
 }
 Enemy::~Enemy(){
 	
--- Enemy.h
+++ Enemy.h
@@ -2,12 +2,17 @@
 #define ENEMY_H
 #include "character.h"
 #include "Controls.h"
+#include "Map.h"
+#include "Utility.h"
 class Enemy :public Character{
 public:
 	Enemy();
 	float sizeX, sizeY;
 	int type;
-	void update(float dt, float playerposX, float playerposY);
+	void jump();
+	void applyGravity();
+	void checkCollision(Map &ptr, sf::View);
+	void update(float dt, float playerposX, float playerposY, Map &ptr, sf::View view);
 	void create(float posX, float posY, int type);
 	~Enemy();
 };
