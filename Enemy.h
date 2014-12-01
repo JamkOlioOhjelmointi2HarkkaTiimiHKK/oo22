e9b568f92b471d812e2294ec51d42c4e3f086b01
JoonasKatainen <H3248@student.jamk.fi>
2014-12-01 15:16:10 +0200

    Revert "Spooky ghost update 1.2"
    This reverts commit c5056da343a627a2aff9e9f9eac1b705bfd1084e.

--- Enemy.h
+++ Enemy.h
@@ -2,17 +2,12 @@
 #define ENEMY_H
 #include "character.h"
 #include "Controls.h"
-#include "Map.h"
-#include "Utility.h"
 class Enemy :public Character{
 public:
 	Enemy();
 	float sizeX, sizeY;
 	int type;
-	void jump();
-	void applyGravity();
-	void checkCollision(Map &ptr, sf::View);
-	void update(float dt, float playerposX, float playerposY, Map &ptr, sf::View view);
+	void update(float dt, float playerposX, float playerposY);
 	void create(float posX, float posY, int type);
 	~Enemy();
 };
