4614638ca3b641657fb28c36d573687a800ee9aa
ArttuHeinonen <arttu.hei@gmail.com>
2014-12-03 17:57:02 +0200

    Pikanen enemy editti

--- Enemy.cpp
+++ Enemy.cpp
@@ -186,20 +186,17 @@ void Enemy::jump(){
 	
 }
 void Enemy::checkCollision(Map &ptr, sf::View view){
-
 	
-
-	if (isEnemyOnAdjacentSector(ptr, view)){		
-		bottom = sprite.getPosition() + sf::Vector2f(0, sizeY / 2);
-		for (int i = 0; i < ptr.xRivi[enemySectorX][enemySectorY].size(); ++i)
-		{
-			if (!ptr.xRivi[enemySectorX][enemySectorY][i]->passable){
-				if (ptr.xRivi[enemySectorX][enemySectorY][i]->shape.getGlobalBounds().contains(bottom)){
-					cout << "succes!" << endl;
-				}
+	bottom = sprite.getPosition() + sf::Vector2f(0, sizeY / 2);
+	for (int i = 0; i < ptr.xRivi[enemySectorX][enemySectorY].size(); ++i)
+	{
+		if (!ptr.xRivi[enemySectorX][enemySectorY][i]->passable){
+			if (ptr.xRivi[enemySectorX][enemySectorY][i]->shape.getGlobalBounds().contains(bottom)){
+				//cout << "succes!" << endl;
 			}
 		}
 	}
+	
 
 	hitbox.update(sprite);
 		
